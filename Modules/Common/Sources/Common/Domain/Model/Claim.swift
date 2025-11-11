c6cad44717f746cb35c31c3097a8c0aa8ac96bf1
Luthfi Asmara <dev.luthfiasmara@gmail.com>
2025-11-11 20:38:16 +0700

    feat(claim): add init data claim

--- Modules/Common/Sources/Common/Domain/Model/Claim.swift
+++ Modules/Common/Sources/Common/Domain/Model/Claim.swift
@@ -12,4 +12,11 @@ public struct Claim: Codable {
   public let id: Int?
   public let title: String?
   public let body: String?
+  
+  public init(userId: Int? = nil, id: Int? = nil, title: String? = nil, body: String? = nil) {
+    self.userId = userId
+    self.id = id
+    self.title = title
+    self.body = body
+  }
 }
